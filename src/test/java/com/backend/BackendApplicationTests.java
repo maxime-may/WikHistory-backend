892535cb57b99fe88ede8a76e657c2999f1fac93
johnny <maxime.may@etu.estia.fr>
2025-09-26 16:24:36 +0200

    fix cicd frontend

--- src/test/java/com/backend/BackendApplicationTests.java
+++ src/test/java/com/backend/BackendApplicationTests.java
@@ -3,7 +3,6 @@ package com.backend;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BackendApplicationTests {
